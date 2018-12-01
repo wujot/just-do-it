6957bbb00ea69393d011bce0f499fb7590daffbf
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2018-12-01 17:26:14 +0100

    Add Admin Panel Style Guide

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+'use strict';
 
+// Get the container element
+var btnContainer = document.getElementById("navbar-content");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+} 
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("navbar-dropdown").classList.toggle("show");
+}
